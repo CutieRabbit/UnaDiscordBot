c23e1ca8f6f48bf3faae64ce0452b60a6249278e
CutieRabbit <police71134@gmail.com>
2020-07-12 20:39:06 +0800

    把<remind功能補回
    Signed-off-by: CutieRabbit <police71134@gmail.com>

--- src/main/java/sigtuna/discord/event/NormalEvent.java
+++ src/main/java/sigtuna/discord/event/NormalEvent.java
@@ -2,13 +2,16 @@ package sigtuna.discord.event;
 
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.message.MessageAuthor;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
+import sigtuna.discord.exception.EmbedException;
 import sigtuna.discord.function.RemoveMessage;
 import sigtuna.discord.function.ServerInfo;
 import sigtuna.discord.main.Main;
+import sigtuna.discord.module.Remind;
 
 public class NormalEvent implements MessageCreateListener {
 
@@ -20,13 +23,24 @@ public class NormalEvent implements MessageCreateListener {
         String prefix = Main.prefix;
 
         if(message.getAuthor().isYourself()) return;
-        if(!message.getAuthor().isBotOwner()) return;
+        if(message.isPrivateMessage()) return;
+
+        TextChannel textChannel = message.getChannel();
 
         if(split[0].equals(prefix + "<<") && split.length == 2){
-            message.removeContentAndEmbed();
-            int count = Integer.parseInt(split[1]);
-            RemoveMessage removeMessage = new RemoveMessage();
-            removeMessage.clearMessage(message, count);
+            try {
+                MessageAuthor author = message.getAuthor();
+                if (author.isBotOwner()) {
+                    message.delete();
+                    int count = Integer.parseInt(split[1]);
+                    RemoveMessage removeMessage = new RemoveMessage();
+                    removeMessage.clearMessage(message, count);
+                } else {
+                    throw new EmbedException(textChannel, "權限不足", "這個功能僅限Bot主人使用");
+                }
+            }catch (EmbedException e){
+                e.print();
+            }
         }
 
         if(split[0].equalsIgnoreCase(prefix + "serverInfo")){
@@ -34,6 +48,11 @@ public class NormalEvent implements MessageCreateListener {
             EmbedBuilder embedBuilder = serverInfo.getServerInfoEmbed(message);
             TextChannel channel = message.getChannel();
             channel.sendMessage(embedBuilder);
+        }
+
+        if(split[0].equalsIgnoreCase(prefix + "remind")){
+            Remind remind = new Remind();
+            remind.execute(event);
         }
     }
 }
